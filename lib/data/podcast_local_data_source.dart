a21e68ade57cbc4b2ed534daefd81feb2b5848a0
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2020-10-01 21:01:21 +0200

    Create concrete local data source implemetation

--- lib/data/podcast_local_data_source.dart
+++ lib/data/podcast_local_data_source.dart
@@ -3,3 +3,10 @@ import 'package:webfeed/webfeed.dart';
 abstract class PodcastLocalDataSource {
   Future<RssFeed> getPodcast(String url);
 }
+
+class PodcastLocalDataSourceImpl implements PodcastLocalDataSource {
+  PodcastLocalDataSourceImpl();
+
+  @override
+  Future<RssFeed> getPodcast(String url) => null;
+}
