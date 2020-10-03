fec33608ea60697df9ea695853d4959893be7336
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2020-10-03 16:48:07 +0200

    Test get podcast feed use case

---
+++ test/domain/get_podcast_feed_usecase_test.dart
@@ -0,0 +1,45 @@
+import 'package:castaway/domain/episode.dart';
+import 'package:castaway/domain/get_podcast_feed_usecase.dart';
+import 'package:castaway/domain/podcast_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockPodcastRepository extends Mock implements PodcastRepository {}
+
+void main() {
+  GetPodcastFeedUseCase usecase;
+  MockPodcastRepository mockRepository;
+
+  setUp(() {
+    mockRepository = MockPodcastRepository();
+    usecase = GetPodcastFeedUseCase(mockRepository);
+  });
+
+  List<Episode> episodes = List(2);
+  episodes[0] = Episode(
+    title: "Episode 1",
+    description: "Descriptions of Episode 1",
+  );
+  episodes[1] = Episode(
+    title: "Episode 2",
+    description: "Descriptions of Episode 2",
+  );
+
+  final podcastUrl = "podcast.url";
+
+  test(
+    'should get list of episodes for the feed url from the repository',
+    () async {
+      // given
+      when(mockRepository.getPodcastFeed(any))
+          .thenAnswer((_) async => Right(episodes));
+      // when
+      final result = await usecase.execute(Params(url: podcastUrl));
+      // then
+      expect(result, Right(episodes));
+      verify(mockRepository.getPodcastFeed(podcastUrl));
+      verifyNoMoreInteractions(mockRepository);
+    },
+  );
+}
