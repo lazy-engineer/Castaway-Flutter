7919fc3f1ad36ac7f69b90efd737f43eb6ed26e6
lazy-engineer <52445213+lazy-engineer@users.noreply.github.com>
2020-10-04 19:13:34 +0200

    Extract podcast feed page to extra file

---
+++ lib/presentation/page/podcast_feed_page.dart
@@ -0,0 +1,54 @@
+import 'package:castaway/data/podcast_local_data_source.dart';
+import 'package:castaway/data/podcast_remote_data_source.dart';
+import 'package:castaway/domain/get_podcast_feed_usecase.dart';
+import 'package:castaway/domain/podcast_repository.dart';
+import 'package:castaway/presentation/widget/episodes_screen.dart';
+import 'package:castaway/presentation/widget/error_screen.dart';
+import 'package:castaway/presentation/widget/fetch_screen.dart';
+import 'package:castaway/presentation/widget/loading_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+
+import '../podcast_bloc.dart';
+import '../podcast_state.dart';
+
+class PodcastFeedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(context),
+    );
+  }
+
+  BlocProvider<PodcastBloc> _buildBody(BuildContext context) {
+    final _remote = PodcastRemoteDataSourceImpl(client: http.Client());
+    final _local = PodcastLocalDataSourceImpl();
+    final _repository = PodcastRepositoryImpl(
+      remoteDataSource: _remote,
+      localDataSource: _local,
+    );
+    final _getPodcastFeed = GetPodcastFeedUseCase(_repository);
+
+    return BlocProvider(
+      create: (_) => PodcastBloc(getPodcastFeed: _getPodcastFeed),
+      child: Center(
+        child: BlocBuilder<PodcastBloc, PodcastState>(
+          // ignore: missing_return
+          builder: (context, state) {
+            if (state is Empty) {
+              return FetchScreen();
+            } else if (state is Loading) {
+              return LoadingScreen();
+            } else if (state is Loaded) {
+              return PodcastFeedScreen(episodes: state.episodes);
+            } else if (state is Error) {
+              return ErrorScreen(message: state.message);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
